9a4af9bbf85d78cd0f9d84d0fc88bb0646769c17
Arvinshu <shuxinwei@gmail.com>
2016-09-10 13:22:30 +0800

    add junit 4

---
+++ src/test/java/com/arvin/model/UserEntityTest.java
@@ -0,0 +1,185 @@
+package com.arvin.model;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* UserEntity Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>dec 10, 2016</pre>
+* @version 1.0 
+*/ 
+public class UserEntityTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getId() 
+* 
+*/ 
+@Test
+public void testGetId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setId(int id) 
+* 
+*/ 
+@Test
+public void testSetId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getNickname() 
+* 
+*/ 
+@Test
+public void testGetNickname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setNickname(String nickname) 
+* 
+*/ 
+@Test
+public void testSetNickname() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPassword() 
+* 
+*/ 
+@Test
+public void testGetPassword() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPassword(String password) 
+* 
+*/ 
+@Test
+public void testSetPassword() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getFirstName() 
+* 
+*/ 
+@Test
+public void testGetFirstName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setFirstName(String firstName) 
+* 
+*/ 
+@Test
+public void testSetFirstName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLastName() 
+* 
+*/ 
+@Test
+public void testGetLastName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLastName(String lastName) 
+* 
+*/ 
+@Test
+public void testSetLastName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getEmail() 
+* 
+*/ 
+@Test
+public void testGetEmail() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setEmail(String email) 
+* 
+*/ 
+@Test
+public void testSetEmail() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(Object o) 
+* 
+*/ 
+@Test
+public void testEquals() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hashCode() 
+* 
+*/ 
+@Test
+public void testHashCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getBlogsById() 
+* 
+*/ 
+@Test
+public void testGetBlogsById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setBlogsById(Collection<BlogEntity> blogsById) 
+* 
+*/ 
+@Test
+public void testSetBlogsById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
